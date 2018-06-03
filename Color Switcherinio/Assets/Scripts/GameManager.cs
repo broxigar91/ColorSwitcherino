af0841625dad671b90e0e7b676075717d677d4f2
broxigar91 <ichigobiar@gmail.com>
2018-06-03 14:09:16 +0200

    Sprites added + some logic
    Player jump coded
    Circle rotation coded
    GameManager v0.1

---
+++ Color Switcherinio/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+	// Use this for initialization
+	void Awake ()
+    {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Color Switcherinio/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidBody.velocity = Vector2.up * speed;
+        }
+    }
+}
